eb4062182bfd147c97afff353c7c740052c04652
liusong1111 <liusong1111@gmail.com>
2014-12-25 00:39:04 +0800

    fetch 环信的token

--- src/ths/huanxin.clj
+++ src/ths/huanxin.clj
@@ -5,30 +5,35 @@
             [clj-time.core :as t]
             [taoensso.timbre :as logger]))
 
-(def token-info (atom nil))
+(declare token-info fetch-token)
 
 (defn get-token []
   (let [ti @token-info
-        {:keys [token expire-at status]} ti
-        ]
-    (if (> expire-at t/now)
+        {:keys [token expire-at fetcher]} ti]
+    (if (or (not token) (not expire-at) (t/after? (t/now) expire-at))
+      (do
+        @(:fetcher ti)
+        ;(get-token)
+        (recur)
+        )
       token
-      (fetch-token)
       )
     )
-  (let [{:keys [timestamp token]} (@-cached-token)]
-    (if (or (not timestamp) (<= (t/plus timestamp (t/days 5)) (t/now))))
-    (get-token))
   )
 
 (defn get-token-callback [{:keys [status headers body error opts] :as response}]
   (if error
-    (swap token-info :status :done)
+    (do
+      (logger/error "error while fetching token")
+      (swap! token-info assoc :fetcher (fetch-token))
+      )
+
     (let [data (json/parse-string body true)
-          {:keys [access_token expires_in]}]
-      (swap token-info :status :done
-            :expire-at (t/plus (t/now) (t/seconds expires_in) (t/minutes -10))
-            :token access_token)
+          {:keys [access_token expires_in]} data]
+      (swap! token-info assoc
+             :fetcher nil
+             :expire-at (t/plus (t/now) (t/seconds expires_in) (t/minutes -10))
+             :token access_token)
       )
     ))
 
@@ -43,10 +48,15 @@
                                               :client_secret "YXA62xaG_k1OsmSdGYtjIKE3XbO0ahw"
                                               })
               }
-             (fn [{:keys [status headers body error] :as response}]
-               (println body)
-               ))
+             get-token-callback
+             )
   )
 
+(def token-info (atom {:fetcher (fetch-token)}))
+
 (defn -main []
-  (get-token))
+  (println "preparing...")
+  (println (get-token))
+  ;(println)
+  ;(println (t/after? (t/now) (t/plus (t/now) (t/seconds 3) (t/minutes -10))))
+  (println "ok!"))
