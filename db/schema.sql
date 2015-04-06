a7e73a5ed557497c6b687d04a4d0a42c0fc2ec17
liusong1111 <liusong1111@gmail.com>
2015-04-06 15:32:57 +0800

    schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -9,6 +9,8 @@ CREATE TABLE users (
   city             char(255),
   image            char(255),
   huanxin_username char(255),
+  isonline         char(255) DEFAULT '离线',
+  status           char(255) DEFAULT '正常',
   created_at       timestamp DEFAULT current_timestamp,
   last_login_at    timestamp
 );
@@ -66,3 +68,44 @@ CREATE TABLE feedbacks (
   content    TEXT,
   created_at timestamp DEFAULT current_timestamp
 );
+
+CREATE TABLE isolatelog (
+  id            INT(11) PRIMARY KEY,
+  admin_id      INT(11),
+  user_id       INT(11),
+  operated_time timestamp DEFAULT CURRENT_TIMESTAMP,
+  begin_time    timestamp DEFAULT CURRENT_TIMESTAMP,
+  end_time      timestamp DEFAULT CURRENT_TIMESTAMP,
+  reason        TEXT
+);
+
+CREATE TABLE admins (
+  id           INT(11) PRIMARY KEY,
+  idcard       char(32),
+  worker_id    INT(11),
+  adder        char(255),
+  username     char(255),
+  password     char(255),
+  provence     char(255),
+  power        INT(10),
+  email        char(255),
+  sex          char(255),
+  phone        char(255),
+  picture      char(255),
+  forget_time  timestamp,
+  forget_token char(255),
+  created_at   timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE system_config (
+  id                   INTEGER PRIMARY KEY,
+  can_register_user    BOOLEAN DEFAULT TRUE,
+  can_login                    DEFAULT TRUE,
+  can_upgrade_silently TRUE
+);
+
+INSERT INTO admins (idcard, worker_id, adder, username, password, provence, power, email, sex, phone, picture)
+VALUES ('111', 1, 'root', 'admin', 'admin', 'TJ', 1, 'admin@tonghang.com', '男', '111', '11.jpg');
+
+
+INSERT INTO system_config (can_register_user, can_login, can_upgrade_silently) VALUES (1, 1, 0);
